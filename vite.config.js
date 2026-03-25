766233e79477a9a44995b64e39619bb53fec5529
Binoy Changmha <binoy.changmha@gmail.com>
2026-03-25 08:40:09 +0600

    bango

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,15 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-
       manifest: {
-        theme_color: '#121212',  // rgb(18,18,18) in hex — controls browser UI color
-        background_color: '#121212',  // CHANGED: match too, shown during splash screen
         name: "Binoy's Calculator",
         short_name: 'Calculator',
         description: 'Idea. Code. Innovate.',
-        theme_color: '#0f172a',
-        background_color: '#ffffff',
+        theme_color: '#121212',
+        background_color: '#121212',
         display: 'standalone',
         orientation: 'portrait',
         start_url: '/',
-
         icons: [
           {
             src: '/icons/icon-192.png',
